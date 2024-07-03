b1424e68c2b68000266f95d0b67902a82d24977d
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-07-02 22:00:43 -0300

    JS: Array - Métodos de Exclusão e Inclusão

---
+++ Introdução/array/array.js
@@ -0,0 +1,15 @@
+let cesta = ["Banana", "Maçã"];
+
+// Incluindo itens no início do Array
+cesta.unshift("Pêra");
+
+// Incluindo ao final do Array
+cesta.push("Uva");
+
+// Excluindo itens no início do Array
+cesta.pop();
+
+// Excluindo itens no começo do Array
+cesta.shift();
+
+console.log(cesta);
