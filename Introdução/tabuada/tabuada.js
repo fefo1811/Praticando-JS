498aa89840c494ab21f343ed9786e9db9766c25b
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-07-08 14:27:28 -0300

    JS: Atividade Tabuada usando While & Percorrendo Array com For

--- Introdução/tabuada/tabuada.js
+++ Introdução/tabuada/tabuada.js
@@ -5,3 +5,15 @@ for(let y = 1; y <= 10; y++) {
     }
     console.log('==========')
 }
+
+// while
+let x = 1;
+while(x <= 10) {
+    let y = 0;
+    while (y <= 10) {
+        console.log(`${x} x ${y} = ${x * y}`)
+        y++
+    }
+    console.log('===========')
+    x++
+}
