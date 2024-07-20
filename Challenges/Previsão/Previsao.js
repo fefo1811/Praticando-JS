035544aa8ec0e18e46f02f4657229b2efdce9eee
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-07-20 20:24:08 -0300

    Challenge JS: Testando o desafio com a estrutura For

--- Challenges/Previsão/Previsao.js
+++ Challenges/Previsão/Previsao.js
@@ -1,9 +1,17 @@
 function exibirTermometro(arr) {
-    let counter = 0
+    // let counter = 0
+    //
+    // while(counter < arr.length) {
+    //     console.log(`${arr[counter]}ºC in ${counter + 1} days`);
+    //     counter++
+    // }
+    // console.log('============')
 
-    while(counter < arr.length) {
-        console.log(`${arr[counter]}ºC in ${counter + 1} days`);
-        counter++
+    for (let i = 0; i < arr.length; i++) {
+        let word = (i + 1) < 2 ? "day" : "days";
+
+
+        console.log(`${arr[i]}ºC in ${i + 1} ${word}`);
     }
     console.log('============')
 }
