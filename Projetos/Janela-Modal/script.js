1dc2f5291846e2c21460a9c1f43595d1cdbc4bf4
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-07-25 22:32:13 -0300

    JS: Criando Modal com JS

---
+++ Projetos/Janela-Modal/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < showModal.length; i++) {
+
+}
