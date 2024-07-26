de7a14148f1fc58fd36bfc64fb5d60fa216ea01f
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-07-26 10:33:38 -0300

    JS: Manipulando classes CSS com .classList

--- Projetos/Janela-Modal/script.js
+++ Projetos/Janela-Modal/script.js
@@ -5,6 +5,20 @@ const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close-modal');
 const showModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < showModal.length; i++) {
-
+// Função para esconder o Modal
+const hideModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 }
+
+// Loop para poder adicionar o EventListener em cada um dos 3 botões da tela
+for (let i = 0; i < showModal.length; i++) {
+    showModal[i].addEventListener('click', () => {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+}
+
+closeBtn.addEventListener('click', hideModal)
+overlay.addEventListener('click', hideModal)
+
